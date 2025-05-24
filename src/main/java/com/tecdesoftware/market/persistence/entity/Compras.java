cd662f127895a444ccafdce30143a38be9eb1bde
EierMayo <e.g.edier.may@gmail.com>
2025-05-24 13:28:15 -0600

    Adding two simple entities

--- src/main/java/com/tecdesoftware/market/persistence/entity/Compras.java
+++ src/main/java/com/tecdesoftware/market/persistence/entity/Compras.java
@@ -5,27 +5,84 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table (name = "compras")
+
+@Table (name="compras")
+
 public class Compras {
 
-    @Id
-    //valor único
+    @Id//Llave primaria
+    //Hace el ID autoincremental
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_compra")
-
+    @Column(name ="id_compra")
     private Integer idCompra;
 
-    @Column(name = "codigo_cliente")
-    private String id_Cliente;
+    private String nombre;
+    @Column(name="id_cliente")
+    private Integer idCategoria;
 
     private LocalDateTime fecha;
 
-    @Column(name = "medio_pago")
-    private String medioPago;
+    @Column(name="medio_pago")
+    private Double medioPago;
 
-    private String comentario;
-    private String estado;
+
+    private Integer comentario;
+
+    private Boolean estado;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public Double getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(Double medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public Integer getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(Integer comentario) {
+        this.comentario = comentario;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
-
-
-
