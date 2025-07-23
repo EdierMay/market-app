6783328fd42debdddf92d3da8c88edb74a89d923
EierMayo <e.g.edier.may@gmail.com>
2025-07-23 08:57:05 -0600

    feat:allow CORS from all controller

--- src/main/java/com/tecdesoftware/market/persistence/mapper/PurchaseItemMapper.java
+++ src/main/java/com/tecdesoftware/market/persistence/mapper/PurchaseItemMapper.java
@@ -2,6 +2,7 @@ package com.tecdesoftware.market.persistence.mapper;
 
 import com.tecdesoftware.market.domain.PurchaseItem;
 import com.tecdesoftware.market.persistence.entity.CompraProducto;
+import com.tecdesoftware.market.persistence.entity.Producto;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -20,10 +21,16 @@ public interface PurchaseItemMapper {
 
     @InheritInverseConfiguration
     @Mappings({
+            @Mapping(source = "productId", target = "id.idProducto"),
             @Mapping(target="id.idCompra", ignore=true),
             @Mapping(target="compra", ignore=true),
             @Mapping(target="producto", ignore=true)
     })
     CompraProducto toCompraProducto(PurchaseItem item);
 
+    default Producto createProductoFromId(int productId) {
+        Producto producto = new Producto();
+        producto.setIdProducto(productId);
+        return producto;
+    }
 }
--- src/main/java/com/tecdesoftware/market/web/controller/PurchaseController.java
+++ src/main/java/com/tecdesoftware/market/web/controller/PurchaseController.java
@@ -1,39 +1,90 @@
-
 package com.tecdesoftware.market.web.controller;
 
 import com.tecdesoftware.market.domain.Purchase;
 import com.tecdesoftware.market.domain.service.PurchaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-
 
 @RestController
-@RequestMapping("purchase")
+@RequestMapping("/purchase")
+@Tag(name = "Purchase Controller", description = "Manage client purchases")
 public class PurchaseController {
 
     @Autowired
     private PurchaseService purchaseService;
 
     @GetMapping("/all")
+    @Operation(
+            summary = "Get all purchases",
+            description = "Returns a list of all purchases made"
+    )
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved all purchases")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<List<Purchase>> getAll() {
         return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/client/{idClient}")
-    public ResponseEntity<List<Purchase>> getByClient(@PathVariable("idClient") String clientId) {
+    @Operation(
+            summary = "Get purchases by client ID",
+            description = "Returns all purchases made by a specific client"
+    )
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved purchases for client")
+    @ApiResponse(responseCode = "404", description = "No purchases found for this client")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    public ResponseEntity<List<Purchase>> getByClient(
+            @Parameter(description = "Client ID to retrieve purchases for", example = "abc123", required = true)
+            @PathVariable("idClient") String clientId) {
         return purchaseService.getByClient(clientId)
                 .map(purchases -> new ResponseEntity<>(purchases, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-
     @PostMapping("/save")
-    public ResponseEntity< Purchase> save(@RequestBody Purchase purchase) {
+    @Operation(
+            summary = "Save a new purchase",
+            description = "Creates a new purchase with products and client details",
+            requestBody = @RequestBody(
+                    required = true,
+                    description = "Purchase to be created",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "Example Purchase",
+                                    value = """
+                                    {
+                                        "clientId": "abc123",
+                                        "products": [
+                                            {
+                                                "productId": 1,
+                                                "quantity": 2
+                                            },
+                                            {
+                                                "productId": 3,
+                                                "quantity": 1
+                                            }
+                                        ]
+                                    }
+                                    """
+                            )
+                    )
+            )
+    )
+    @ApiResponse(responseCode = "201", description = "Purchase created successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid purchase data")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    public ResponseEntity<Purchase> save(@org.springframework.web.bind.annotation.RequestBody Purchase purchase) {
         return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
     }
 }
